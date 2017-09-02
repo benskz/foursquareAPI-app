e6526209cbc65f5b6360cb5326c5395a2ad19a28
benskz <bernardo@unlockingthedigital.com>
2017-09-02 16:41:38 +0100

    added api endpoint path builder

--- src/foursquare/index.js
+++ src/foursquare/index.js
@@ -4,11 +4,22 @@ class Foursquare {
   constructor () {
     this.clientId = 'BGQ1EX1WJXJK1EDLFTL2TEBZEJSGWR1DJ43KKE1ZBEGC4IDD'
     this.clientSecret = 'X3P5TJWTRUPRN1YYCSRA0QNACEOOOMV5UJ4EC55MZV143C3T'
+    this.url = 'https://api.foursquare.com/v2/'
+    this.v = '20170901'
+  }
+
+  api (endpoint, parameters) {
+    let path = `${this.url + endpoint}?`
+
+    Object.keys(parameters).forEach((key) => {
+      path += `${key}=${parameters[key]}&`
+    })
+
+    return `${path}client_id=${this.clientId}&client_secret=${this.clientSecret}&v=${this.v}`
   }
 
   search (location, callback) {
-    console.log(location)
-    request(`https://api.foursquare.com/v2/venues/explore?near=${location}&client_id=${this.clientId}&client_secret=${this.clientSecret}&v=20170901`, (error, response, body) => {
+    request(this.api('venues/explore', { near: location }), (error, response, body) => {
       if (!error && response && response.statusCode === 200) callback(JSON.parse(body).response)
     })
   }

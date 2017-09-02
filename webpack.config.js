a9da6f238493f5b9c367ccc2c12d078b913b500e
benskz <bernardo@unlockingthedigital.com>
2017-09-02 15:25:17 +0100

    project dependencies and skeleton

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: './src/main.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    modules: ['src', 'node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                autoprefixer: false,
+                sourceMap: true,
+                importLoaders: 1,
+              },
+            },
+            'postcss-loader',
+            'sass-loader',
+          ],
+        },
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                autoprefixer: false,
+                sourceMap: true,
+                importLoaders: 1,
+              },
+            },
+            'postcss-loader',
+            'sass-loader',
+          ],
+        }),
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.json$/,
+        use: 'json-loader',
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+    new HtmlWebpackPlugin({
+      title: 'Foursquare API',
+      template: 'index.ejs',
+    }),
+  ],
+  devServer: {
+    contentBase: './dist',
+  },
+}
